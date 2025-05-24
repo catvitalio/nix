c8b2c213fd01883138a9ffe0b95cde638937b7f5
Vitaly Kotov <catvitalio@gmail.com>
2025-05-24 21:50:22 +0700

    fix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   description = "Multi-host configuration";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
@@ -7,16 +8,17 @@
     disko.url = "github:nix-community/disko";
   };
 
-  outputs = { nixpkgs, flake-utils, disko, nixos-hardware, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-      in { packages.default = pkgs.hello; })
+  outputs = { self, nixpkgs, flake-utils, disko, nixos-hardware, ... }:
+    let supportedSystems = [ "x86_64-linux" "aarch64-darwin" ];
+    in {
+      packages = flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in { default = pkgs.hello; });
 
-    {
       nixosConfigurations = {
         sid = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
@@ -28,14 +30,14 @@
           ];
         };
 
-        joey = nixpkgs.lib.nixosSystem {
-          system = "aarch64-darwin";
+        joey = nixpkgs.lib.darwinSystem {
+          system =
+            "aarch64-darwin"; # Внимание: nixosSystem используется только для Linux!
           modules = [
             ./machines/joey/configuration.nix
             nixos-hardware.nixosModules.apple-macbook
           ];
         };
       };
-
     };
 }
